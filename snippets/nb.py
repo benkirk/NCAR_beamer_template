3810fa59f02401776c513f510456249a05579059
Benjamin Kirk <benjamin.s.kirk@gmail.com>
2022-04-15 10:42:27 -0400

    more sourcecode styling

---
+++ snippets/nb.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+if rank == 0:
+    data = {'a': 7, 'b': 3.14}
+    req = comm.isend(data, dest=1, tag=11)
+    req.wait()
+elif rank == 1:
+    req = comm.irecv(source=0, tag=11)
+    data = req.wait()
